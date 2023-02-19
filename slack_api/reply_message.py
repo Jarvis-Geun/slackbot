ebe2464e19b336612bb155fdc90d9d0407b28619
jarvis.geun <jarvis.geun@gmaill.com>
2023-02-19 19:23:47 +0900

    reply_message

--- slack_api/reply_message.py
+++ slack_api/reply_message.py
@@ -2,19 +2,21 @@ from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
 
-def send_message(client, channel_id, message, ts_path):
+def reply_messag(client, channel_id, message, ts_path):
+    # Load timestamp for reply
+    with open(ts_path, "r") as f:
+        message_ts = f.readlines()[-1]
+        print(message_ts)
+
     try:
         # Call the chat.postMessage method using the WebClient
         # The client passes the token you included in initialization    
         result = client.chat_postMessage(
             channel=channel_id,
             text=message,
+            thread_ts=message_ts
             # You could also use a blocks[] array to send richer content
         )
-        ts_path = "<path of the timestamp>"
-        with open(ts_path, "a") as f:
-            f.write(result['ts'] + '\n')
-            print(result['ts'])
 
     except SlackApiError as e:
         print(f"Error: {e}")
@@ -28,4 +30,4 @@ if __name__=="__main__":
 
     client = WebClient(myToken)
 
-    send_message(client=client, channel_id=channel_id, message=message, ts_path=ts_path)
+    reply_message(client=client, channel_id=channel_id, message=message, ts_path)
